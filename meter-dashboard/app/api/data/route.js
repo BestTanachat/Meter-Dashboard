3b7c92a141f3cd6370a0ee3aa87b7e7754e259db
BestTanachat <tanachat.photilaksap@gmail.com>
2024-03-11 18:38:18 +0700

    deploy

--- meter-dashboard/app/api/data/route.js
+++ meter-dashboard/app/api/data/route.js
@@ -38,44 +38,40 @@
 //   }
 // };
 
-
 // app/api/data/route.js
-import { InfluxDB } from '@influxdata/influxdb-client';
+import { InfluxDB } from "@influxdata/influxdb-client";
 
 export const GET = async (request) => {
-    try {
-        const influxDB = new InfluxDB({
-            url: process.env.INFLUX_URL,
-            token: process.env.INFLUX_TOKEN,
-        });
-        const queryApi = influxDB.getQueryApi(process.env.INFLUX_ORG);
-        const query = `
+  try {
+    const influxDB = new InfluxDB({
+      url: process.env.INFLUX_URL,
+      token: process.env.INFLUX_TOKEN,
+    });
+    const queryApi = influxDB.getQueryApi(process.env.INFLUX_ORG);
+    const query = `
     from(bucket: "${process.env.INFLUX_BUCKET}")
     |> range(start: -1d)
     |> filter(fn: (r) => r._measurement == "wifi_status")
 `;
 
+    const rows = await queryApi.collectRows(query);
+    const response = [];
 
-            const rows = await queryApi.collectRows(query);
-            const response = [];
-        
-            for await (const row of rows) {
-              console.log(row._value)
-                    const temperature = row._value || '';
-                    response.push({ temperature:temperature});
-                  }
-
-        return new Response(JSON.stringify(response), {
-            status: 200,
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-    } catch (error) {
-        console.error(error);
-        return new Response(error, {
-            status: 500
-        });
+    for await (const row of rows) {
+      const temperature = row._value || "";
+      response.push({ temperature: temperature });
     }
+
+    return new Response(JSON.stringify(response), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(error, {
+      status: 500,
+    });
+  }
 };
-  
